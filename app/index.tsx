c52e78aa598f22b8e73df6802e3c5d514fa94cfd
Marcio Bueno <marciobueno@gmail.com>
2025-03-11 21:47:25 +0000

    depois de resetar a aplicação exemplo

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
-export default function AppScreen() {
+export default function Index() {
   return (
-    <View style={styles.view}>
-      <View style={{ backgroundColor: "yellow" }}>
-        <Text style={styles.title}>Olá Turma!</Text>
-        <Text style={styles.text}>Um texto normal / um parágrafo!</Text>
-      </View>
-      <View style={{ backgroundColor: "yellowgreen" }}>
-        <Text style={styles.footer}>Rodapé</Text>
-      </View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "beige",
-  },
-  title: {
-    color: "darkGray",
-    fontSize: 50,
-  },
-  text: {
-    color: "darkGray",
-    fontSize: 15,
-  },
-  footer: {
-    color: "darkGray",
-    fontSize: 10,
-  },
-});
